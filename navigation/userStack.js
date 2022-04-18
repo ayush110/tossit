b22839fbd8b67be039145f7909c27f00b5ff960e
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-04-18 10:14:43 -0400

    basic tab-navigation

--- navigation/userStack.js
+++ navigation/userStack.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from '../screens/Home';
+import Search from '../screens/Search';
+import Profile from '../screens/Profile';
 
-const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
 
 function UserStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'Search') {
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+              }
+              else if (route.name === 'Profile') {
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Search" component={Search} />
+          <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    
   );
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useAuthentication } from '../utils/hooks/useAuthentication';
-import { Button } from 'react-native-elements';
+import { Button, Tab } from 'react-native-elements';
 import { getAuth, signOut } from 'firebase/auth';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from './Search';
+import Profile from './Profile';
+
+const Stack = createStackNavigator();
 
 export default function HomeScreen() {
   const { user } = useAuthentication();
   const auth = getAuth();
+  
   return (
     <View >
+      
       <Text>Welcome {user?.email}!</Text>
 
       <Button title="Sign Out" onPress={() => signOut(auth)}/>
